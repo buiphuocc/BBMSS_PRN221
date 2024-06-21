4bcbba82c686c56da78c2cf9fe4b9225cd36ff07
tnmd130103 <tnmd130103@gmail.com>
2024-06-21 13:43:23 +0700

    fix nav/side bar change format schedule,create booking form

---
+++ BBMSRazorPages/Pages/Booking/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BusinessObjects;
+
+namespace BBMSRazorPages.Pages.Booking
+{
+    public class CreateModel : PageModel
+    {
+        private readonly BusinessObjects.BadmintonBookingSystemContext _context;
+
+        public CreateModel(BusinessObjects.BadmintonBookingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CourtId"] = new SelectList(_context.Courts, "CourtId", "CourtName");
+        ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email");
+            return Page();
+        }
+
+        [BindProperty]
+        public BusinessObjects.Booking Booking { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Bookings == null || Booking == null)
+            {
+                return Page();
+            }
+
+            _context.Bookings.Add(Booking);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ BBMSRazorPages/Pages/Booking/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects;
+
+namespace BBMSRazorPages.Pages.Booking
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BusinessObjects.BadmintonBookingSystemContext _context;
+
+        public IndexModel(BusinessObjects.BadmintonBookingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public IList<BusinessObjects.Booking> Booking { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Bookings != null)
+            {
+                Booking = await _context.Bookings
+                .Include(b => b.Court)
+                .Include(b => b.User).ToListAsync();
+            }
+        }
+    }
+}
