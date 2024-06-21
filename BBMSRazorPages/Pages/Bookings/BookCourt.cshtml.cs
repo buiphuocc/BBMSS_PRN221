5bf80707c027f35f76654f959e651aa5934ee0ea
BuiPhuoc <146042853+buiphuocc@users.noreply.github.com>
2024-06-21 14:08:27 +0700

    latest

---
+++ BBMSRazorPages/Pages/Bookings/BookCourt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BBMSRazorPages.Pages.Bookings
+{
+    public class BookCourtModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
