15ea9efc66ccdcbb79a6ad8c636f2b6de01f2e1b
tnmd130103 <tnmd130103@gmail.com>
2024-07-06 16:19:59 +0700

    fix background login,css dashboard

--- BBMSRazorPages/Pages/Dashboard.cshtml.cs
+++ BBMSRazorPages/Pages/Dashboard.cshtml.cs
@@ -9,19 +9,25 @@ namespace BBMSRazorPages.Pages
     {
         private readonly IBookingService _bookingService;
         private readonly IBookingServiceService bookingServiceService;
+        private readonly ICourtService _courtService;
 
-        public DashboardModel(IBookingService bookingService, IBookingServiceService bookingServiceService)
+
+        public DashboardModel(IBookingService bookingService, IBookingServiceService bookingServiceService,ICourtService courtService)
         {
             _bookingService = bookingService;
             this.bookingServiceService = bookingServiceService;
+            _courtService = courtService;
         }
 
         public IList<BusinessObjects.Booking> Bookings { get; set; } = new List<BusinessObjects.Booking>();
         public IList<BusinessObjects.BookingService> BookingServices { get; set; } = new List<BusinessObjects.BookingService>();
+        public IList<BusinessObjects.Court> Courts { get; set; } = new List<BusinessObjects.Court>();
+
 
         public IActionResult OnGet()
         {
             Bookings = _bookingService.GetAllBookings();
+            Courts = _courtService.GetAllCourts();
             BookingServices = bookingServiceService.GetAllBookingServices();
             return Page();
         }
