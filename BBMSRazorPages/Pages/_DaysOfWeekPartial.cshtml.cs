dc314bca56848e050c90a168b32b892d379928cf
Khoa Nguyễn <khoa.nguyen090703@gmail.com>
2024-07-11 23:29:44 +0700

    feat: Schedule booking interface

---
+++ BBMSRazorPages/Pages/_DaysOfWeekPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BBMSRazorPages.Pages
+{
+    public class _DaysOfWeekPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
